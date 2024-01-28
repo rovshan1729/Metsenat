06bbff7030deb603b7492a89b178f0c52f22d4ac
Rovshan <super777imba@gmail.com>
2024-01-28 16:24:51 +0500

    login page

--- accounts/views.py
+++ accounts/views.py
@@ -53,6 +53,9 @@ class UserLoginView(generics.CreateAPIView):
 class RegisterUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
